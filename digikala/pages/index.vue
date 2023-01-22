c7938ad909ed7bbf5246618c501f2c7002f895e1
Maryam_rshn <mari.r9777@gmail.com>
2023-01-22 12:57:29 +0330

    navbar component created and added to home page

---
+++ digikala/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  components: {
+    Navbar:() => import('@/components/header/navbar.vue')
+  }
+}
+</script>
+<style>
+* {
+  font-family: iranyekan,sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
