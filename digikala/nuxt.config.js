9ceded79ee8c910861f007f0de4b131a6208418f
Maryam_rshn <mari.r9777@gmail.com>
2023-01-23 12:24:38 +0330

    installing scss

--- digikala/nuxt.config.js
+++ digikala/nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    "@/assets/global.css"
+    "@/assets/global.css",
+    '@/assets/scss/global.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -46,5 +47,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
   }
 }
