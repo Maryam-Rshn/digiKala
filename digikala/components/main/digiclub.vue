4a375ce9086cf38c672f7432d93600f36f690a66
Maryam_rshn <mari.r9777@gmail.com>
2023-02-06 15:26:24 +0330

    digiclub component created and added to home page

---
+++ digikala/components/main/digiclub.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="digiclub-container">
+    <img class="digiclub-logo" src="https://www.digikala.com/statics/img/svg/club/digiclub-logo-white.svg" width="119px" height="33px" alt="">
+    <div class="digiclub-links">
+        <div class="digiclub-link" v-for="link in digiClub" :key="link.text">
+            <div class="text">
+                <svg v-html="link.svg_path" style="width: 20px; height: 20px;"></svg>
+                <span>{{ link.text }}</span>
+            </div>
+            <img :src="link.img_url" width="168px" height="60px" alt="">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        digiClub: Array,
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/global.scss';
+
+.digiclub-container {
+    margin: 8px 64px 0;
+    padding: 12px 16px;
+    border-radius: 16px;
+    background: linear-gradient(76.02deg,#35a6c8 33.32%,#007295 71.55%),radial-gradient(21.29% 87.33% at 50% 50.67%,#7cf2f6 0,#35a6c8 100%);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    direction: rtl;
+}
+.digiclub-logo {
+    margin-right: 24px;
+}
+.digiclub-links {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    :first-child {
+        border-radius: 0 8px 8px 0;
+    }
+    :last-child {
+        border-radius: 8px 0 0 8px;
+    }
+}
+.digiclub-link {
+    display: flex;
+    align-items: center;
+    background-color: white;
+    .text {
+        margin-right: 16px;
+        display: flex;
+        align-items: center;
+        svg {
+            fill: $light-blue;
+            :only-child {
+                transform: scale(.8);
+            }
+        }
+        span {
+            font-size: 12px;
+            color: $dark-blue-button;
+            font-weight: 700;
+            margin-right: 8px;
+        }
+    }
+}
+</style>
--- digikala/pages/index.vue
+++ digikala/pages/index.vue
@@ -45,6 +45,7 @@
         </div>
       </mostVisited>
       <specialDelivery :specialDeliveries="specialDeliveries" />
+      <digiclub :digiClub="digiClub"/>
     </main>
   </div>
 </template>
@@ -75,6 +76,23 @@ export default {
       thirdBanner: [
         {url: 'https://dkstatics-public.digikala.com/digikala-adservice-banners/b2440fd28e00b20d23107b067b8ee418a8ac9396_1674636606.jpg?x-oss-process=image/quality,q_95', width: '644px'},
         {url: 'https://dkstatics-public.digikala.com/digikala-adservice-banners/95519d402c9db4c317e06d0f70e2870ad11ced7f_1674847897.jpg?x-oss-process=image/quality,q_95', width: '644px'}
+      ],
+      digiClub: [
+        {
+          svg_path: '<path fill-rule="evenodd" d="M15.9 4.888C15.492 3.22 13.888 2 12 2c-1.894 0-3.504 1.228-3.904 2.906l-.006-.014-.27.136a9 9 0 108.08-.14zm-.174 2.305l.034-.099a7 7 0 11-7.523 0c.11.312.253.617.43.916a6 6 0 106.667 0c.157-.267.289-.54.392-.817zm-1.645 2.39l.137-.137-.136.137h-.001zm0 0a4 4 0 11-4.162.001 9.608 9.608 0 001.544 1.26l.537.341.537-.341a9.612 9.612 0 001.545-1.26zM10 5.723C10 4.792 10.873 4 12 4s2 .792 2 1.722c0 .725-.444 1.533-1.218 2.331a8.16 8.16 0 01-.515.487L12 8.762l-.267-.222a8.16 8.16 0 01-.515-.487C10.444 7.255 10 6.447 10 5.723z" clip-rule="evenodd"></path>',
+          text: 'چرخ و بخت',
+          img_url: 'https://www.digikala.com/statics/img/png/digiclub/wheel-spinner.jpg'
+        },
+        {
+          svg_path: '<path fill-rule="evenodd" d="M18.994 1.888l.006.114v3h3c.813 0 1.256.898.848 1.534l-.065.089-.076.084-3 3a1 1 0 01-.576.284l-.131.009h-3.586l-3.707 3.707a1 1 0 01-1.497-1.32l.083-.094L14 8.586V5.002a1 1 0 01.206-.608l.087-.1 3-3c.575-.574 1.523-.252 1.684.486l.017.108zm-6.493 2.239a9 9 0 107.375 7.376l-2.037-.001A7 7 0 1112.5 6.163V4.127zm-.205 4.045A5 5 0 1016 13.002l-.01-.307a4.984 4.984 0 00-.137-.9l-.024-.087-1.888 1.89a3.001 3.001 0 11-3.536-3.537l1.89-1.889zM16 5.416l1-1v1.586l.007.117.02.112a1 1 0 00.973.771h1.584l-1 1H16V5.416z" clip-rule="evenodd"></path>',
+          text: 'ماموریت ها',
+          img_url: 'https://www.digikala.com/statics/img/png/digiclub/missions.png'
+        },
+        {
+          svg_path: '<path fill-rule="evenodd" d="M10 4h10a3 3 0 013 3v10a3 3 0 01-3 3H10a1 1 0 01-1-1 1 1 0 10-2 0 1 1 0 01-1 1H4a3 3 0 01-3-3V7a3 3 0 013-3h2a1 1 0 011 1 1 1 0 002 0 1 1 0 011-1zM8 8a3.001 3.001 0 01-2.83-2H4a1 1 0 00-1 1v10a1 1 0 001 1h1.17a3.001 3.001 0 015.66 0H20a1 1 0 001-1V7a1 1 0 00-1-1h-9.17A3.001 3.001 0 018 8zm5.5 2a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm5.5 5.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm-6.848-.03l1.696 1.06 5-8-1.696-1.06-5 8zM7 15v-2h2v2H7zm0-6v2h2V9H7z" clip-rule="evenodd"></path>',
+          text: 'جایزه ها',
+          img_url: 'https://www.digikala.com/statics/img/png/digiclub/awards.png'
+        }
       ]
     }
   },
@@ -130,6 +148,8 @@ export default {
     popularBrand:() => import('@/components/main/popularBrand.vue'),
     mostVisited:() => import('@/components/main/mostVisited.vue'),
     specialDelivery:() => import('@/components/main/specialDelivery.vue'),
+    digiclub:() => import('@/components/main/digiclub.vue'),
+
 
   },
   async asyncData({store}) {
