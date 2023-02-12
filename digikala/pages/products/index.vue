d1029d5f51e8f4f60f9676ce61418f85a37e0b73
Maryam_rshn <mari.r9777@gmail.com>
2023-02-12 12:19:17 +0330

    the appearance of the products page created

---
+++ digikala/pages/products/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar>
+      <Menu/>
+    </Navbar>
+    <products />
+  </div>
+</template>
+
+<script>
+export default {
+    components: {
+      Navbar:() => import('@/components/header/navbar.vue'),
+      Menu:() => import('@/components/header/Menu.vue'),
+      products:() => import('@/components/main/ProductsTemplate.vue'),
+    }
+
+}
+</script>
+
+<style>
+
+</style>
